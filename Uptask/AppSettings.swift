72b9782fb01a6ea6e2ddb14e80a193a3e5e53f38
bulkabuka <amaruelleof@gmail.com>
2023-10-19 17:10:40 +0300

    Initial

---
+++ Uptask/AppSettings.swift
@@ -0,0 +1,8 @@
+//
+//  AppSettings.swift
+//  Uptask
+//
+//  Created by Erich Helvig on 19.10.2023.
+//
+
+import Foundation
---
+++ Uptask/StartScreenView.swift
@@ -0,0 +1,8 @@
+//
+//  StartScreenView.swift
+//  Uptask
+//
+//  Created by Erich Helvig on 18.10.2023.
+//
+
+import Foundation
